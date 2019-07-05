0c52f81c16df4f0d78a853ca467ac0b5852e3f81
amrifaezeh <amrifaezeh@gmail.com>
2019-07-05 19:41:44 +0800

    create login

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const home = require('./routes/home') // Importing the home route
+const register = require('./routes/register')//Import the register route
+const login = require('./routes/login')
+
+mongoose.connect('mongodb://localhost/sample-store', (err, data) => {
+	if (err){
+		console.log('DB Connection Failed')
+		return
+	}
+ 
+	console.log('DB Connection Success')
+})
+const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hjs')
+
+app.use(express.json()) //Parse the form as JSON
+app.use(express.urlencoded({extended: false})) //Let us receive the data and parse it
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', home) // Tell the path to use the route
+app.use('/register', register)//Connect the path and route
+app.use('/login', login)
+
+app.get('/',(req, res, next)=>{
+    res.send('this is the home route')
+
+})
+
+app.listen(5000)
+console.log('App running on http://localhost:5000')
