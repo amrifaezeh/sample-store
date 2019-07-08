7a7e6bd503d73fff8915b6cc16f5c6bcfa9e7b99
amrifaezeh <amrifaezeh@gmail.com>
2019-07-08 23:28:48 +0800

    selecting items

--- routes/account.js
+++ routes/account.js
@@ -28,6 +28,27 @@ router.get('/', (req, res, next) => {
 		res.render('account', data)
 	})
 })
+router.get('/additem/:itemid', (req, res, next) => {
+	const user = req.user
+	if (user == null){
+		res.redirect('/')
+		return
+	}
+	Item.findById(req.params.itemid, (err, item) => {
+		if (err){
+			return next(err)
+		}
+	 
+		if (item.interested.indexOf(user._id) == -1){
+			item.interested.push(user._id)
+			item.save()
+			res.json({
+				item: item
+			})
+		}
+	 
+	})
+})
 router.get('/logout', (req, res, next) => {
 	req.logout()
 	res.redirect('/')
